c3e62f0b28a5d00b50b5aa8cbc21985331adee44
Karolina Dys <karolinadyss@gmail.com>
2019-10-25 12:26:58 +0200

    add playerMove & computerMove js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-unction printMessage(msg){
+let computerMove = 'kamień';
+let playerMove = 'papier';
+
+
+function printMessage(msg ''Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!' ){
 	var div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
