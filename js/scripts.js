ad88be0ff00e0e461739687599efc4538409b1d5
Karolina Dys <karolinadyss@gmail.com>
2019-10-25 11:55:50 +0200

    structure html & js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+unction printMessage(msg){
+	var div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
